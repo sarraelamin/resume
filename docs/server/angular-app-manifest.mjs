8824236302b6590b320a1b01fdc0c5e30aff9844
Harithelamin <harithelamin@gmail.com>
2025-01-19 18:33:17 +0100

    fix the pdf file
    fix the pdf file

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 510, hash: 'aa6ecab1392802f11794c49c06e8664fdc704e98195f7cad8e7aecb0fe604ea7', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1023, hash: '84154644d0ae3d0d2d1f014a9a8727d5efc0609f6a997dd3fffa72b751c28404', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 510, hash: '8142eeed55eadcd6d1b38550a443736ead70fbb0ef5a034db85f50e4cee04708', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1023, hash: '317b4b59f25d47f2616bfd0bfe6b6b01aa2a1b750825fd66ba7f6331db10afc2', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
